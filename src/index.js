fe58c5b8e9cbeac12fde01c403663fc3cce0c1e6
VladyslavPrytula <vladyslavprytula.dev@gmail.com>
2020-08-01 03:26:46 +0300

    added hw-11-colorSwitch

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './styles.css';
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let intervalId;
+let isActive = false;
+console.log(colors.length);
+const changeColor = () => {
+  const randomColor = colors[randomIntegerFromInterval(0, colors.length - 1)];
+  document.body.style.backgroundColor = randomColor;
+};
+changeColor();
+startBtnRef.addEventListener('click', () => {
+  if (isActive) {
+    return;
+  }
+  isActive = true;
+  intervalId = setInterval(() => {
+    changeColor();
+  }, 1000);
+});
+stopBtnRef.addEventListener('click', () => {
+  clearInterval(intervalId);
+  isActive = false;
+});
